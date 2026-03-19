c8fce005c3d39290b740bcc029be74f53091b892
ada3012 <mendozahernandezadair@gmail.com>
2026-03-18 22:22:47 -0600

    wasa

--- php/config.php
+++ php/config.php
@@ -5,9 +5,9 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 try {
     $conn = new mysqli("localhost", "root", "", "bd_compilines_2");
     $conn->set_charset("utf8mb4"); // 'utf8mb4' is the modern standard for emojis/special chars
-    echo "✅ Conexión exitosa"; 
+    echo "Conexión echa por el wasasin de Adair"; 
 } catch (mysqli_sql_exception $e) {
-    error_log($e->getMessage()); // Logs the error privately
+    error_log($e->getMessage()); 
     exit("Error: " . $e->getMessage()); 
 }
 ?>
