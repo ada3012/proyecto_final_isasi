437347cedc161566519b46e7daaea1c963a045c1
Nestorzyzy23 <nangeles.innovacion@gmail.com>
2026-04-23 08:35:25 -0600

    actua

--- js/AM/menu.js
+++ js/AM/menu.js
@@ -8,11 +8,12 @@ class Menu extends HTMLElement {
         @import url("./css/AM/fonts.css");
 
         menu {
-          background-color: #2c3e50;
-          padding: 10px 0;
+          background-color: #319115;
+          padding: 0px 0;
           box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
           margin: 0;
           position: relative;
+              border-radius: 67px;
         }
 
         .menu-toggle {
